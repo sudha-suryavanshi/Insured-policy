bbbe62591da4c5ef9c5850c1e500918def6130ca
sudha-suryavanshi <sudha.suryavanshi@gmail.com>
2026-03-04 19:51:04 +0000

    Create PolicyApplication.java

---
+++ src/main/java/com/policy/PolicyApplication.java
@@ -0,0 +1,12 @@
+package com.policy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyApplication {
+  public static void main(String[] args) {
+    System.out.println("Main java application!!!");
+    SpringApplication.run(PolicyApplication.class, args);
+  }
+}
