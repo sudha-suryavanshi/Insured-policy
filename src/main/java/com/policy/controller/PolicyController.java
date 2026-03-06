80b17a3827960709802111a96310df9badfa9e50
sudha-suryavanshi <sudha.suryavanshi@gmail.com>
2026-03-06 19:53:29 +0000

    Update PolicyController.java

--- src/main/java/com/policy/controller/PolicyController.java
+++ src/main/java/com/policy/controller/PolicyController.java
@@ -1,16 +1,41 @@
 package com.policy.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.policy.dto.*;
+import com.policy.service.PolicyService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
 
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/policies")
+public class PolicyController {
 
-public class PolicyController { 
-  @GetMapping("/")
-  public String index() {
-      System.out.println("PolicyController!!!!");
-    return "Greetings from Spring Boot!";
-  }
- 
+ private final PolicyService service;
+
+ public PolicyController(PolicyService service){
+   this.service = service;
+ }
+
+ @PostMapping
+ @ResponseStatus(HttpStatus.CREATED)
+ public PolicyResponse create(@Valid @RequestBody PolicyCreateRequest req){
+   return service.create(req);
+ }
+
+ @GetMapping
+ public List<PolicyResponse> list(){
+   return service.list();
+ }
+
+ @GetMapping("/{id}")
+ public PolicyResponse get(@PathVariable String id){
+   return service.get(id);
+ }
+
+ @PostMapping("/{id}/cancel")
+ public PolicyResponse cancel(@PathVariable String id){
+   return service.cancel(id);
+ }
 }
