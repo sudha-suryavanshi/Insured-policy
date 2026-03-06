f9faba6ed3d883e788ee081bec22a49f32e68d7c
sudha-suryavanshi <sudha.suryavanshi@gmail.com>
2026-03-06 19:07:53 +0000

    updated the controller

--- src/main/java/com/policy/controller/PolicyController.java
+++ src/main/java/com/policy/controller/PolicyController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class HelloWorldController { 
+public class PolicyController { 
   @GetMapping("/")
   public String index() {
-      System.out.println("HelloWorldController-Hello!!!!!!");
+      System.out.println("PolicyController!!!!");
     return "Greetings from Spring Boot!";
   }
  
